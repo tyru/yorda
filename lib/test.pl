7b86a7018f004f26d3d439c6ef15bafe54a79db1
tyru <tyru.exe@gmail.com>
2018-10-13 09:19:55 +0900

    WIP: fix tests

--- lib/test.pl
+++ lib/test.pl
@@ -1,170 +1,122 @@
 :- consult(vimscript).
 :- use_module(vimscript).
 
+:- discontiguous test/1, test/2.
+
 :- begin_tests(syntax_error).
-  test(no_such_term) :- \+ eval(foobar, _, _).
+  test(no_such_term, [fail]) :- eval(foobar, _, _).
 :- end_tests(syntax_error).
 
 :- begin_tests(primitive_types).
-  test(tAny) :-
-    Value = tAny @ [1, 1],
-    eval(Value, _, Value).
-  test(tVoid) :-
-    Value = tVoid @ [1, 1],
-    eval(Value, _, Value).
-  test(tInt) :-
-    Value = tInt(_) @ [1, 1],
-    eval(Value, _, Value).
-  test(tFloat) :-
-    Value = tFloat(_) @ [1, 1],
-    eval(Value, _, Value).
-  test(tString) :-
-    Value = tString(_) @ [1, 1],
-    eval(Value, _, Value).
-  test(tList) :-
-    Value = tList(_) @ [1, 1],
-    eval(Value, _, Value).
-  test(tDict) :-
-    Value = tDict(_) @ [1, 1],
-    eval(Value, _, Value).
-  test(tTuple0) :-
-    Value = tTuple([]) @ [1, 1],
-    eval(Value, _, Value).
-  test(tTuple1) :-
-    Value = tTuple([tInt(42) @ [1, 2]]) @ [1, 1],
-    eval(Value, _, Value).
-  test(tTuple2) :-
-    Value = tTuple([tInt(42) @ [1, 2], tFloat(12.34) @ [1, 3]]) @ [1, 1],
-    eval(Value, _, Value).
+  test(tAny, [true(R = tAny @ [1, 1])]) :-
+    eval(tAny @ [1, 1], _, R).
+  test(tVoid, [true(R = tVoid @ [1, 1])]) :-
+    eval(tVoid @ [1, 1], _, R).
+  test(tInt, [true(R = tInt(_) @ [1, 1])]) :-
+    eval(tInt(_) @ [1, 1], _, R).
+  test(tFloat, [true(R = tFloat(_) @ [1, 1])]) :-
+    eval(tFloat(_) @ [1, 1], _, R).
+  test(tString, [true(R = tString(_) @ [1, 1])]) :-
+    eval(tString(_) @ [1, 1], _, R).
+  test(tList, [true(R = tList(_) @ [1, 1])]) :-
+    eval(tList(_) @ [1, 1], _, R).
+  test(tDict, [true(R = tDict(_) @ [1, 1])]) :-
+    eval(tDict(_) @ [1, 1], _, R).
+  test(tTuple0, [true(R = tTuple([]) @ [1, 1])]) :-
+    eval(tTuple([]) @ [1, 1], _, R).
+  test(tTuple1, [true(R = tTuple([tInt(42) @ [1, 2]]) @ [1, 1])]) :-
+    eval(tTuple([tInt(42) @ [1, 2]]) @ [1, 1], _, R).
+  test(tTuple2, [true(R = tTuple([tInt(42) @ [1, 2], tFloat(12.34) @ [1, 3]]) @ [1, 1])]) :-
+    eval(tTuple([tInt(42) @ [1, 2], tFloat(12.34) @ [1, 3]]) @ [1, 1], _, R).
 
-  test(not_equal) :-
-    eval(tInt(42) @ nopos, _, R), \+ R = tInt(999) @ nopos.
-  test(non_string_dict_key) :-
-    eval(tDict([tInt(42) @ [1,2] : tInt(42) @ [1,3]]) @ [1,1], _, R),
-    R = tError('key is not string')@[1, 2].
+  test(non_string_dict_key, [true(R = tError('key is not string')@[1, 2])]) :-
+    eval(tDict([tInt(42) @ [1,2] : tInt(42) @ [1,3]]) @ [1,1], _, R).
 :- end_tests(primitive_types).
 
 :- begin_tests(expr).
   % $ENV is string
-  test(env_is_string) :-
-    eval(
-      env("ENV") @ [1,1],
-      _,
-      tString(_) @ [1,1]).
+  test(env_is_string, [true(R = tString(_) @ [1,1])]) :-
+    eval(env("ENV") @ [1,1], _, R).
   % @a is string
-  test(reg_is_string) :-
-    eval(
-      reg("a") @ [1,1],
-      _,
-      tString(_) @ [1,1]).
+  test(reg_is_string, [true(R = tString(_) @ [1,1])]) :-
+    eval(reg("a") @ [1,1], _, R).
   % &expandtab
-  test(option_expandtab) :-
-    eval(
-      option("expandtab") @ [1,1],
-      _,
-      tInt(_) @ [1,1]).
+  test(option_expandtab, [true(R = tBool(_) @ [1,1])]) :-
+    eval(option("expandtab") @ [1,1], _, R).
   % &et
-  test(option_et) :-
-    eval(
-      option("et") @ [1,1],
-      _,
-      tInt(_) @ [1,1]).
+  test(option_et, [true(R = tBool(_) @ [1,1])]) :-
+    eval(option("et") @ [1,1], _, R).
   % &unknown
-  test(option_unknown) :-
-    eval(option("unknown") @ [1,1], _, R),
-    R = tError('no such option: unknown') @ [1,1].
+  test(option_unknown, [true(R = tError('no such option: unknown') @ [1,1])]) :-
+    eval(option("unknown") @ [1,1], _, R).
   % abs(42) == 42
-  test(call_abs_int) :-
-    eval(
-      call(ident("","abs") @ nopos, [tInt(42) @ nopos]) @ [1,1],
-      _,
-      tInt(42) @ [1,1]).
+  test(call_abs_int, [true(R = tInt(42) @ [1,1])]) :-
+    Expr = call(ident("","abs") @ nopos, [tInt(42) @ [1,1]]) @ [1,1],
+    eval(Expr, _, R).
   % abs(12.34) == 12.34
-  test(call_abs_float) :-
-    eval(
-      call(ident("","abs") @ nopos, [tFloat(12.34) @ nopos]) @ [1,1],
-      _,
-      tFloat(12.34) @ [1,1]).
+  test(call_abs_float, [true(R = tFloat(12.34) @ [1,1])]) :-
+    Expr = call(ident("","abs") @ nopos, [tFloat(12.34) @ [1,1]]) @ [1,1],
+    eval(Expr, _, R).
   % abs(<tInt>) != <tFloat>
-  test(call_abs_fail1) :-
-    eval(
-      call(ident("","abs") @ nopos, [tInt(42) @ nopos]) @ [1,1],
-      _,
-      R),
-    \+ R = tFloat(_) @ [1,1].
+  test(call_abs_fail1, [fail]) :-
+    Expr = call(ident("","abs") @ nopos, [tInt(42) @ [1,1]]) @ [1,1],
+    eval(Expr, _, R),
+    R = tFloat(_) @ [1,1].
   % abs(<tFloat>) != <tInt>
-  test(call_abs_fail2) :-
-    eval(
-      call(ident("","abs") @ nopos, [tFloat(12.34) @ nopos]) @ [1,1],
-      _,
-      R),
-    \+ R = tInt(_) @ [1,1].
+  test(call_abs_fail2, [fail]) :-
+    Expr = call(ident("","abs") @ nopos, [tFloat(12.34) @ [1,1]]) @ [1,1],
+    eval(Expr, _, R),
+    R = tInt(_) @ [1,1].
   % acos(0.0) == 1.570796
-  test(call_acos) :-
-    eval(
-      call(ident("","acos") @ nopos, [tFloat(0) @ nopos]) @ [1,1],
-      _,
-      tFloat(1.570796) @ [1,1]).
+  test(call_acos, [true(R = tFloat(_) @ [1,1])]) :-
+    Expr = call(ident("","acos") @ nopos, [tFloat(0) @ [1,1]]) @ [1,1],
+    eval(Expr, _, R).
   % add([], 42) == [42]
-  test(call_add) :-
-    eval(
-      call(ident("","add") @ nopos, [tList([]) @ nopos, tInt(42) @ [1,5]]) @ [1,1],
-      _,
-      tList([tInt(42) @ [1,5]]) @ [1,1]).
+  test(call_add, [true(R = tList([tInt(42) @ [1,5]]) @ [1,1])]) :-
+    Expr = call(ident("","add") @ nopos, [tList([]) @ [1,1], tInt(42) @ [1,5]]) @ [1,1],
+    eval(Expr, _, R).
   % and(1, 1) == 1
-  test(call_and) :-
-    eval(
-      call(ident("","and") @ nopos, [tInt(1) @ nopos, tInt(1) @ nopos]) @ [1,1],
-      _,
-      tInt(1) @ [1,1]).
+  test(call_and, [true(R = tInt(1) @ [1,1])]) :-
+    Expr = call(ident("","and") @ nopos, [tInt(1) @ nopos, tInt(1) @ [1,1]]) @ [1,1],
+    eval(Expr, _, R).
   % append(1, "foo") == 0
-  test(call_append) :-
-    eval(
-      call(ident("","append") @ nopos, [tInt(1) @ nopos, tString("foo") @ nopos]) @ [1,1],
-      _,
-      tInt(0) @ [1,1]).
+  test(call_append, [true(R = tInt(0) @ [1,1])]) :-
+    Expr = call(ident("","append") @ nopos, [tInt(1) @ nopos, tString("foo") @ nopos]) @ [1,1],
+    eval(Expr, _, R).
   % append(".", "foo") == 0
-  test(call_append) :-
-    eval(
-      call(ident("","append") @ nopos, [tString(".") @ nopos, tString("foo") @ nopos]) @ [1,1],
-      _,
-      tInt(0) @ [1,1]).
+  test(call_append, [true(R = tInt(0) @ [1,1])]) :-
+    Expr = call(ident("","append") @ nopos, [tString(".") @ nopos, tString("foo") @ nopos]) @ [1,1],
+    eval(Expr, _, R).
   % append(1, []) == 0
-  test(call_append) :-
-    eval(
-      call(ident("","append") @ nopos, [tInt(1) @ nopos, tList([]) @ nopos]) @ [1,1],
-      _,
-      tInt(0) @ [1,1]).
+  test(call_append, [true(R = tInt(0) @ [1,1])]) :-
+    Expr = call(ident("","append") @ nopos, [tInt(1) @ nopos, tList([]) @ nopos]) @ [1,1],
+    eval(Expr, _, R).
   % append(".", []) == 0
-  test(call_append) :-
-    eval(
-      call(ident("","append") @ nopos, [tString(".") @ nopos, tList([]) @ nopos]) @ [1,1],
-      _,
-      tInt(0) @ [1,1]).
+  test(call_append, [true(R = tInt(0) @ [1,1])]) :-
+    Expr = call(ident("","append") @ nopos, [tString(".") @ nopos, tList([]) @ nopos]) @ [1,1],
+    eval(Expr, _, R).
   % has("eval") == 1
-  test(call_has) :-
-    eval(
-      call(ident("","has") @ nopos, [tString("eval") @ nopos]) @ [1,1],
-      _,
-      tInt(1) @ [1,1]).
+  test(call_has, [true(R = tInt(1) @ [1,1])]) :-
+    Expr = call(ident("","has") @ nopos, [tString("eval") @ nopos]) @ [1,1],
+    eval(Expr, _, R).
 :- end_tests(expr).
 
 :- begin_tests(node).
   % file(Excmds)
-  test(file1, all(R = [tSuccess])) :-
+  test(file1, [true(R = tSuccess)]) :-
     eval(file([]) @ [1,1], _, R).
-  test(file2, all(R = [tSuccess])) :-
+  test(file2, [true(R = tSuccess)]) :-
     eval(file([comment("comment") @ nopos]) @ [1,1], _, R).
 
   % comment(Text)
-  test(comment, all(R = [tSuccess])) :-
+  test(comment, [true(R = tSuccess)]) :-
     eval(comment("this is a comment") @ [1,1], _, R).
 :- end_tests(node).
 
 :- begin_tests(excmds).
   % let n = 42
   % echo n
-  test(n_is_int) :-
+  test(n_is_int, [true(Results = [(ident("g", "n"), Pos, Rhs)])]) :-
     Var = ident("", "n"),
     Pos = [1, 5],
     Rhs = tInt(42) @ [1, 9],
